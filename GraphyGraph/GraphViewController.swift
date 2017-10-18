fe78e578d6713641fcd2d997d749302f157b4d4b
Rick Sanchez <peter.prokop@blackswan.com>
2017-10-18 11:48:37 +0100

    Add GraphViewController

---
+++ GraphyGraph/GraphViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GraphViewController.swift
+//  GraphyGraph
+//
+//  Created by Peter Prokop on 18/10/2017.
+//  Copyright © 2017 Peter Prokop. All rights reserved.
+//
+
+import Cocoa
+
+class GraphViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .cyan
+    }
+    
+}
--- GraphyGraph/MainViewController.swift
+++ GraphyGraph/MainViewController.swift
@@ -16,15 +16,67 @@ class MainViewController: NSViewController {
         let view1 = NSView()
         view1.backgroundColor = NSColor.red
 
-        let view2 = NSView()
-        view2.backgroundColor = NSColor.green
+        let graphViewController = GraphViewController()
+
+        addChildViewController(graphViewController)
 
         let splitView = NSSplitView(frame: view.bounds)
+        splitView.translatesAutoresizingMaskIntoConstraints = false
+
         splitView.isVertical = true
         splitView.addArrangedSubview(view1)
-        splitView.addArrangedSubview(view2)
+        splitView.addArrangedSubview(graphViewController.view)
+
+        splitView.setPosition(100, ofDividerAt: 0)
 
         view.addSubview(splitView)
+
+        let constraint1 = NSLayoutConstraint(
+            item: splitView,
+            attribute: .left,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .left,
+            multiplier: 1,
+            constant: 0
+        )
+
+        let constraint2 = NSLayoutConstraint(
+            item: splitView,
+            attribute: .right,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .right,
+            multiplier: 1,
+            constant: 0
+        )
+
+        let constraint3 = NSLayoutConstraint(
+            item: splitView,
+            attribute: .top,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .top,
+            multiplier: 1,
+            constant: 0
+        )
+
+        let constraint4 = NSLayoutConstraint(
+            item: splitView,
+            attribute: .bottom,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .bottom,
+            multiplier: 1,
+            constant: 0
+        )
+
+        view.addConstraints([
+            constraint1,
+            constraint2,
+            constraint3,
+            constraint4,
+        ])
     }
     
 }
